849979639daac01d8b7f2cb7396a0e7507d9e9c5
Ashmoz <95120906+Ashmoz@users.noreply.github.com>
2022-03-17 19:55:05 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(random(255),random(255),random(255))
+  ellipse(100,100,100,100)
+  //wait(516410000)
+  noLoop();
+}
+
+
+function wait(timeout) {
+    for(let i=0;i<timeout;i++){
+      "f"
+    }
+}
